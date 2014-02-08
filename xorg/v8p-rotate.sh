42856a74910df8fed59cc92ba3bc69a72c8c8c0b
Adam Williamson <awilliam@redhat.com>
2014-02-07 20:19:52 -0800

    add v8p-rotate script for doing manual screen rotation on v8p

---
+++ xorg/v8p-rotate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+xinputs=( 9 10 )
+
+xrandrout="$(xrandr)"
+
+case $xrandrout in
+ *1280?x?800* ) rotate=0;;
+ *800?x?1280* ) rotate=1;;
+esac
+
+xrandr -o $(( rotate * 3 ))
+for input in ${xinputs[@]}; do
+ xinput set-prop $input "Evdev Axes Swap" $rotate
+ xinput set-prop $input "Evdev Axis Inversion" 0, $rotate
+done
